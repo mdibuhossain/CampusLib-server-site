0444bdcc1cfcb617784f0c8d340285de079d9354
Ibrahim H <mohammad.ihakash@gmail.com>
2022-06-18 00:36:42 +0600

    feat: jwt added

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const cors = require('cors');
 const { MongoClient, ObjectId } = require('mongodb');
 require('dotenv').config();
 
+const admin = require("firebase-admin");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
 const app = express();
 const port = process.env.PORT || 5000;
 
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+})
 
 // middleware
 app.use(cors());
@@ -13,6 +19,20 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5p7yt.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const verityToken = async (req, res, next) => {
+    const token = req.headers['authorization'];
+    if (!token) {
+        return res.status(401).send('Unauthorized');
+    }
+    try {
+        const finalToken = token.split(' ')[1];
+        const decodedToken = await admin.auth().verifyIdToken(finalToken);
+        req.decodeEmail = decodedToken.email;
+        next();
+    } catch (e) {
+        return res.status(401).send('Unauthorized');
+    }
+}
 
 async function run() {
     try {
@@ -45,10 +65,22 @@ async function run() {
         })
 
         // get users
-        app.get('/users', async (req, res) => {
-            const cursor = usersCollection.find({});
-            const result = await cursor.toArray();
-            res.json(result);
+        app.get('/users', verityToken, async (req, res) => {
+            const requester = req.decodeEmail;
+            if (requester) {
+                const requesterAccount = await usersCollection.findOne({ email: requester });
+                if (requesterAccount?.role === 'admin') {
+                    const cursor = usersCollection.find({});
+                    const result = await cursor.toArray();
+                    res.json(result);
+                }
+                else {
+                    res.status(401).send('Unauthorized');
+                }
+            }
+            else {
+                res.status(401).send('Unauthorized');
+            }
         })
 
         // Check admin
@@ -91,98 +123,200 @@ async function run() {
         })
 
         // make user admin
-        app.put('/user/makeadmin/:id', async (req, res) => {
+        app.put('/user/makeadmin/:id', verityToken, async (req, res) => {
             const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const options = { upsert: true };
-            const update = { $set: { role: "admin" } };
-            const result = await usersCollection.updateOne(query, update, options);
-            res.json(result);
+            const requester = req.decodeEmail;
+            if (requester) {
+                const requesterAccount = await usersCollection.findOne({ email: requester });
+                if (requesterAccount?.role === 'admin') {
+                    const query = { _id: ObjectId(id) };
+                    const options = { upsert: true };
+                    const update = { $set: { role: "admin" } };
+                    const result = await usersCollection.updateOne(query, update, options);
+                    res.json(result);
+                }
+                else {
+                    res.json({ error: "You are not admin" });
+                }
+            }
+            else {
+                res.status(401).send('Unauthorized');
+            }
         })
 
         // update content
-        app.put('/update_book/:id', async (req, res) => {
-            const id = req.params.id;
-            const data = req.body;
-            delete data._id;
-            const query = { _id: ObjectId(id) };
-            const options = { upsert: true };
-            const update = { $set: data };
-            const result = await booksCollection.updateOne(query, update, options);
-            res.json(result);
+        app.put('/update_book/:id', verityToken, async (req, res) => {
+            const requester = req.decodeEmail;
+            if (requester) {
+                const requesterAccount = await usersCollection.findOne({ email: requester });
+                if (requesterAccount?.email) {
+                    const id = req.params.id;
+                    const data = req.body;
+                    delete data._id;
+                    const query = { _id: ObjectId(id) };
+                    const options = { upsert: true };
+                    const update = { $set: data };
+                    const result = await booksCollection.updateOne(query, update, options);
+                    res.json(result);
+                }
+            }
+            else {
+                res.status(401).send('Unauthorized');
+            }
         })
-        app.put('/update_question/:id', async (req, res) => {
-            const id = req.params.id;
-            const data = req.body;
-            delete data._id;
-            const query = { _id: ObjectId(id) };
-            const options = { upsert: true };
-            const update = { $set: data };
-            const result = await questionsCollection.updateOne(query, update, options);
-            res.json(result);
+        app.put('/update_question/:id', verityToken, async (req, res) => {
+            const requester = req.decodeEmail;
+            if (requester) {
+                const requesterAccount = await usersCollection.findOne({ email: requester });
+                if (requesterAccount?.email) {
+                    const id = req.params.id;
+                    const data = req.body;
+                    delete data._id;
+                    const query = { _id: ObjectId(id) };
+                    const options = { upsert: true };
+                    const update = { $set: data };
+                    const result = await questionsCollection.updateOne(query, update, options);
+                    res.json(result);
+                }
+            }
+            else {
+                res.status(401).send('Unauthorized');
+            }
         })
-        app.put('/update_syllabus/:id', async (req, res) => {
-            const id = req.params.id;
-            const data = req.body;
-            delete data._id;
-            const query = { _id: ObjectId(id) };
-            const options = { upsert: true };
-            const update = { $set: data };
-            const result = await syllabusCollection.updateOne(query, update, options);
-            res.json(result);
+        app.put('/update_syllabus/:id', verityToken, async (req, res) => {
+            const requester = req.decodeEmail;
+            if (requester) {
+                const requesterAccount = await usersCollection.findOne({ email: requester });
+                if (requesterAccount?.email) {
+                    const id = req.params.id;
+                    const data = req.body;
+                    delete data._id;
+                    const query = { _id: ObjectId(id) };
+                    const options = { upsert: true };
+                    const update = { $set: data };
+                    const result = await syllabusCollection.updateOne(query, update, options);
+                    res.json(result);
+                }
+            }
+            else {
+                res.status(401).send('Unauthorized');
+            }
         })
 
         // change content status
-        app.put('/book/status/:id', async (req, res) => {
-            const id = req.params.id;
-            const status = req.body;
-            const query = { _id: ObjectId(id) };
-            const options = { upsert: true };
-            const update = { $set: { status: status.status } };
-            const result = await booksCollection.updateOne(query, update, options);
-            res.json(result);
+        app.put('/book/status/:id', verityToken, async (req, res) => {
+            const requester = req.decodeEmail;
+            if (requester) {
+                const requesterAccount = await usersCollection.findOne({ email: requester });
+                if (requesterAccount?.role === 'admin') {
+                    const id = req.params.id;
+                    const status = req.body;
+                    const query = { _id: ObjectId(id) };
+                    const options = { upsert: true };
+                    const update = { $set: { status: status.status } };
+                    const result = await booksCollection.updateOne(query, update, options);
+                    res.json(result);
+                }
+                else {
+                    res.status(401).send('Unauthorized');
+                }
+            }
+            else {
+                res.status(401).send('Unauthorized');
+            }
         })
-        app.put('/question/status/:id', async (req, res) => {
-            const id = req.params.id;
-            const status = req.body;
-            const query = { _id: ObjectId(id) };
-            const options = { upsert: true };
-            const update = { $set: { status: status.status } };
-            const result = await questionsCollection.updateOne(query, update, options);
-            res.json(result);
+        app.put('/question/status/:id', verityToken, async (req, res) => {
+            const requester = req.decodeEmail;
+            if (requester) {
+                const requesterAccount = await usersCollection.findOne({ email: requester });
+                if (requesterAccount?.role === 'admin') {
+                    const id = req.params.id;
+                    const status = req.body;
+                    const query = { _id: ObjectId(id) };
+                    const options = { upsert: true };
+                    const update = { $set: { status: status.status } };
+                    const result = await questionsCollection.updateOne(query, update, options);
+                    res.json(result);
+                }
+                else {
+                    res.status(401).send('Unauthorized');
+                }
+            }
+            else {
+                res.status(401).send('Unauthorized');
+            }
         })
-        app.put('/syllabus/status/:id', async (req, res) => {
-            const id = req.params.id;
-            const status = req.body;
-            const query = { _id: ObjectId(id) };
-            const options = { upsert: true };
-            const update = { $set: { status: status.status } };
-            const result = await syllabusCollection.updateOne(query, update, options);
-            res.json(result);
+        app.put('/syllabus/status/:id', verityToken, async (req, res) => {
+            const requester = req.decodeEmail;
+            if (requester) {
+                const requesterAccount = await usersCollection.findOne({ email: requester });
+                if (requesterAccount?.role === 'admin') {
+                    const id = req.params.id;
+                    const status = req.body;
+                    const query = { _id: ObjectId(id) };
+                    const options = { upsert: true };
+                    const update = { $set: { status: status.status } };
+                    const result = await syllabusCollection.updateOne(query, update, options);
+                    res.json(result);
+                }
+                else {
+                    res.status(401).send('Unauthorized');
+                }
+            }
+            else {
+                res.status(401).send('Unauthorized');
+            }
         })
 
         // delete book
-        app.delete('/book/delete/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const result = await booksCollection.deleteOne(query);
-            res.json(result);
+        app.delete('/book/delete/:id', verityToken, async (req, res) => {
+            const requester = req.decodeEmail;
+            if (requester) {
+                const requesterAccount = await usersCollection.findOne({ email: requester });
+                if (requesterAccount?.email) {
+                    const id = req.params.id;
+                    const query = { _id: ObjectId(id) };
+                    const result = await booksCollection.deleteOne(query);
+                    res.json(result);
+                }
+            }
+            else {
+                res.status(401).send('Unauthorized');
+            }
         })
 
         // delete question
-        app.delete('/question/delete/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const result = await questionsCollection.deleteOne(query);
-            res.json(result);
+        app.delete('/question/delete/:id', verityToken, async (req, res) => {
+            const requester = req.decodeEmail;
+            if (requester) {
+                const requesterAccount = await usersCollection.findOne({ email: requester });
+                if (requesterAccount?.email) {
+                    const id = req.params.id;
+                    const query = { _id: ObjectId(id) };
+                    const result = await questionsCollection.deleteOne(query);
+                    res.json(result);
+                }
+            }
+            else {
+                res.status(401).send('Unauthorized');
+            }
         })
 
         // delete syllabus
-        app.delete('/syllabus/delete/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const result = await syllabusCollection.deleteOne(query);
-            res.json(result);
+        app.delete('/syllabus/delete/:id', verityToken, async (req, res) => {
+            const requester = req.decodeEmail;
+            if (requester) {
+                const requesterAccount = await usersCollection.findOne({ email: requester });
+                if (requesterAccount?.email) {
+                    const id = req.params.id;
+                    const query = { _id: ObjectId(id) };
+                    const result = await syllabusCollection.deleteOne(query);
+                    res.json(result);
+                }
+            }
+            else {
+                res.status(401).send('Unauthorized');
+            }
         })
 
     }
