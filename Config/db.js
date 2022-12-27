3286476546f29efe6331084d59cfa502c2fba8bd
chadnipodder <chadnipodder@gmail.com>
2022-12-28 00:05:55 +0600

    update: custom db name

--- Config/db.js
+++ Config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        const con = await mongoose.connect(process.env.MONGO_URI)
+        const con = await mongoose.connect(process.env.NODE_EVN === "development" ? process.env.MONGO_URI_LOCAL : process.env.MONGO_URI, { dbName: process.env.DB_NAME })
         console.log(`MongoDB successfully connected`)
     } catch {
         console.log(`DB connection failed!!`)
