cdcc4cbb78726b3ea52114d35aeabf2d7a7b6391
Ibrahim Hossain <ibuhossain3@gmail.com>
2023-03-23 01:33:41 +0600

    update: DB connection check status

--- Config/db.js
+++ Config/db.js
@@ -3,10 +3,14 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
     try {
         const con = await mongoose.connect(process.env.NODE_EVN === "development" ? process.env.MONGO_URI_LOCAL : process.env.MONGO_URI, { dbName: process.env.DB_NAME })
-        console.log(`MongoDB successfully connected`)
     } catch {
         console.log(`DB connection failed!!`)
     }
+    const check = mongoose.connection.readyState;
+    if (check == 0) console.log("disconnected");
+    else if (check == 1) console.log("connected");
+    else if (check == 2) console.log("connecting");
+    else if (check == 3) console.log("disconnecting");
 }
 
 module.exports = { connectDB }
